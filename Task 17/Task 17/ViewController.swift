a275cfe4761aad78af12ecc5705c2afb529737bf
Anastasiya Vishnevskaya <anastasiyavishnevskaya@gmail.com>
2019-12-06 20:04:29 +0200

    Added task 17.1

---
+++ Task 17/Task 17/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Task 17
+//
+//  Created by Анастасия Вишневская on 12/5/19.
+//  Copyright © 2019 Anastasiya Vishnevskaya. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
