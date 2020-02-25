e294a864a529706a806869dcc31db805cdb6177b
Anastasiya Vishnevskaya <anastasiyavishnevskaya@gmail.com>
2020-02-25 23:44:10 +0200

    Corrected

---
+++ Task. Who wants to be a millionaire?/Pods/Target Support Files/Pods-Task. Who wants to be a millionaire?/Pods-Task. Who wants to be a millionaire?-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_Task__Who_wants_to_be_a_millionaire_ : NSObject
+@end
+@implementation PodsDummy_Pods_Task__Who_wants_to_be_a_millionaire_
+@end
---
+++ Task. Who wants to be a millionaire?/Pods/Target Support Files/Pods-Task. Who wants to be a millionaire?/Pods-Task. Who wants to be a millionaire?-umbrella.h
@@ -0,0 +1,16 @@
+#ifdef __OBJC__
+#import <UIKit/UIKit.h>
+#else
+#ifndef FOUNDATION_EXPORT
+#if defined(__cplusplus)
+#define FOUNDATION_EXPORT extern "C"
+#else
+#define FOUNDATION_EXPORT extern
+#endif
+#endif
+#endif
+
+
+FOUNDATION_EXPORT double Pods_Task__Who_wants_to_be_a_millionaire_VersionNumber;
+FOUNDATION_EXPORT const unsigned char Pods_Task__Who_wants_to_be_a_millionaire_VersionString[];
+
